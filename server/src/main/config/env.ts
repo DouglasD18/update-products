193bcb289ce137906a763fac04dc0af482ab76c3
Douglinhas <daguiaralcantara@gmail.com>
2023-09-07 23:01:52 -0300

    feat: create the server and the middlewares

--- server/src/main/config/env.ts
+++ server/src/main/config/env.ts
@@ -5,5 +5,6 @@ export default {
   db_user: process.env.DB_USER,
   db_pass: process.env.DB_PASS,
   db_name: process.env.DB_NAME,
-  db_port: Number(process.env.DB_PORT)
+  db_port: Number(process.env.DB_PORT),
+  port: Number(process.env.PORT)
 }
